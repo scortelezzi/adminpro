7d2f84891b87bf1cd2ed66be52b5402d5feb49db
Santiago Cortelezzi <scortelezzi@gmail.com>
2019-02-22 22:49:55 -0300

    Modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { PagenotfoundComponent } from './shared/pagenotfound/pagenotfound.component';
-import { DahsboardComponent } from './pages/dahsboard/dahsboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { FooterComponent } from './shared/footer/footer.component';
 import { RegisterComponent } from './register/register.component';
-
 
 //Rutas
 import { APP_ROUTES } from './app.routes';
-import { PagesComponent } from './pages/pages.component';
+
+//Modulos
+import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    PagenotfoundComponent,
-    DahsboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    FooterComponent,
     RegisterComponent,
-    PagesComponent
   ],
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 
-import { PagesComponent } from 'src/app/pages/pages.component';
-
-import { DahsboardComponent } from 'src/app/pages/dahsboard/dahsboard.component';
+//Componentes
 import { LoginComponent } from 'src/app/login/login.component';
-import { ProgressComponent } from 'src/app/pages/progress/progress.component';
-import { Graficas1Component } from 'src/app/pages/graficas1/graficas1.component';
 import { RegisterComponent } from 'src/app/register/register.component';
 import { PagenotfoundComponent } from 'src/app/shared/pagenotfound/pagenotfound.component';
 
-
 const appRoutes: Routes = [
-    { path: '', 
-      component: PagesComponent,
-      children: [
-        { path: 'dashboard', component: DahsboardComponent},      
-        { path: 'progress', component: ProgressComponent},
-        { path: 'graficas1', component: Graficas1Component},
-        { path: '', redirectTo: './dashboard', pathMatch: 'full' }
-      ],
-    },
+
     { path: 'login', component: LoginComponent},
     { path: 'register', component: RegisterComponent},
     { path: '**', component: PagenotfoundComponent }
